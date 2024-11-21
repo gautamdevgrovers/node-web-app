ee833132f446b5fd606d93977ae25b798bf2d33d
Ubuntu <ubuntu@ip-172-31-14-61.ap-south-1.compute.internal>
2024-11-21 12:41:27 +0000

    changed index content for checking

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World! I changed the content lets see build happens or not');
 });
 
 app.listen(PORT, () => {
